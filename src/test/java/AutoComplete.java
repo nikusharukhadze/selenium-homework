2344ad3c9a4991b58620e1f4149807911368eee7
Nikusha Rukhadze <nikusharukhadze24@gmail.com>
2023-08-21 22:58:12 +0400

    update AutoComplete class

--- src/test/java/AutoComplete.java
+++ src/test/java/AutoComplete.java
@@ -3,6 +3,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -25,7 +27,8 @@ public class AutoComplete {
         driver.navigate().to("https://www.google.com/");
         WebElement search = driver.findElement(By.xpath("//*[@id=\"APjFqb\"]"));
         search.sendKeys("Automation");
-        Thread.sleep(4000);
+        WebDriverWait wait =  new WebDriverWait (driver, 4);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"Alh6id\"]/div[1]/div/ul[@class='G43f7e' and @jsname='bw4e9b']")));
         List<WebElement> values = driver.findElements(By.xpath("//*[@id=\"Alh6id\"]/div[1]/div/ul[@class='G43f7e' and @jsname='bw4e9b']/li"));
         Integer lastIndex = values.size() - 1;
         for (int i = 0; i < values.size(); i++) {
